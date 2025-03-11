b8ed7af14ac09c014f4b4adcbc554bf69fb35663
pinal-flex <pinal.flexsolutions@gmail.com>
2025-03-11 12:50:33 +0530

    text changed ttf to woff2

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener("click", function () {
             const description = this.nextElementSibling;
             const icons = this.querySelector(".icons");
-            const plusIcon = icons.firstElementChild; 
-            const minusIcon = icons.lastElementChild; 
+            const plusIcon = icons.firstElementChild;
+            const minusIcon = icons.lastElementChild;
 
             document.querySelectorAll(".accordion-description").forEach((desc) => {
                 if (desc !== description) {
                     desc.style.maxHeight = "0px";
-                    desc.previousElementSibling.querySelector(".icons").firstElementChild.style.display = "block"; 
-                    desc.previousElementSibling.querySelector(".icons").lastElementChild.style.display = "none"; 
+                    desc.previousElementSibling.querySelector(".icons").firstElementChild.style.display = "block";
+                    desc.previousElementSibling.querySelector(".icons").lastElementChild.style.display = "none";
                 }
             });
 
             if (description.style.maxHeight && description.style.maxHeight !== "0px") {
                 description.style.maxHeight = "0px";
-                plusIcon.style.display = "block";  
-                minusIcon.style.display = "none";  
+                plusIcon.style.display = "block";
+                minusIcon.style.display = "none";
             } else {
                 description.style.maxHeight = description.scrollHeight + "px";
-                plusIcon.style.display = "none"; 
-                minusIcon.style.display = "block";  
+                plusIcon.style.display = "none";
+                minusIcon.style.display = "block";
             }
         });
 
